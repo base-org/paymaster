9410aa41e1bf168aff1beaa50577f60d175f5fde
Michael de Hoog <michael.dehoog@coinbase.com>
2023-11-06 19:49:44 -1000

    Fix tests

--- test/Paymaster.t.sol
+++ test/Paymaster.t.sol
@@ -73,7 +73,7 @@ contract PaymasterTest is Test {
         UserOperation memory userOp = createUserOp();
         signUserOp(userOp);
 
-        vm.expectRevert(createEncodedValidationResult(false, 57098));
+        vm.expectRevert(createEncodedValidationResult(false, 55014));
         entrypoint.simulateValidation(userOp);
     }
 
@@ -83,7 +83,7 @@ contract PaymasterTest is Test {
         userOp.paymasterAndData = abi.encodePacked(address(paymaster), abi.encode(MOCK_VALID_UNTIL, MOCK_VALID_AFTER), r, s, v);
         signUserOp(userOp);
 
-        vm.expectRevert(createEncodedValidationResult(true, 57104));
+        vm.expectRevert(createEncodedValidationResult(true, 55020));
         entrypoint.simulateValidation(userOp);
     }
 
