b0acb139b286313ad055b51b11e67f6d1ca7b277
Michael de Hoog <michael.dehoog@coinbase.com>
2023-11-16 21:32:05 -0800

    Rename Deploy to DeployPaymaster

--- script/DeployPaymaster.s.sol
+++ script/DeployPaymaster.s.sol
@@ -6,7 +6,7 @@ import "../src/Paymaster.sol";
 import "@account-abstraction/interfaces/IEntryPoint.sol";
 
 // This script deploys a Paymaster
-contract Deploy is Script {
+contract DeployPaymaster is Script {
     address entryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
 
     function run() public {
